70b48fbd561916637cb0142f7d300e2107736b12
Thomas BESSOU <thomas.bessou@hotmail.fr>
2023-11-20 01:21:04 +0100

    compression codec test fix when not all features

--- tests/misc.rs
+++ tests/misc.rs
@@ -5,19 +5,39 @@ fn compression_codec_serializes_properly() {
 	let codec = CompressionCodec::Null;
 	let serialized = serde_json::to_string(&codec).unwrap();
 	assert_eq!(serialized, "\"null\"");
-	let codec = CompressionCodec::Deflate;
-	let serialized = serde_json::to_string(&codec).unwrap();
-	assert_eq!(serialized, "\"deflate\"");
-	let codec = CompressionCodec::Bzip2;
-	let serialized = serde_json::to_string(&codec).unwrap();
-	assert_eq!(serialized, "\"bzip2\"");
-	let codec = CompressionCodec::Snappy;
-	let serialized = serde_json::to_string(&codec).unwrap();
-	assert_eq!(serialized, "\"snappy\"");
-	let codec = CompressionCodec::Xz;
-	let serialized = serde_json::to_string(&codec).unwrap();
-	assert_eq!(serialized, "\"xz\"");
-	let codec = CompressionCodec::Zstandard;
-	let serialized = serde_json::to_string(&codec).unwrap();
-	assert_eq!(serialized, "\"zstandard\"");
+
+	#[cfg(feature = "deflate")]
+	{
+		let codec = CompressionCodec::Deflate;
+		let serialized = serde_json::to_string(&codec).unwrap();
+		assert_eq!(serialized, "\"deflate\"");
+	}
+
+	#[cfg(feature = "bzip2")]
+	{
+		let codec = CompressionCodec::Bzip2;
+		let serialized = serde_json::to_string(&codec).unwrap();
+		assert_eq!(serialized, "\"bzip2\"");
+	}
+
+	#[cfg(feature = "snappy")]
+	{
+		let codec = CompressionCodec::Snappy;
+		let serialized = serde_json::to_string(&codec).unwrap();
+		assert_eq!(serialized, "\"snappy\"");
+	}
+
+	#[cfg(feature = "xz")]
+	{
+		let codec = CompressionCodec::Xz;
+		let serialized = serde_json::to_string(&codec).unwrap();
+		assert_eq!(serialized, "\"xz\"");
+	}
+
+	#[cfg(feature = "zstandard")]
+	{
+		let codec = CompressionCodec::Zstandard;
+		let serialized = serde_json::to_string(&codec).unwrap();
+		assert_eq!(serialized, "\"zstandard\"");
+	}
 }
