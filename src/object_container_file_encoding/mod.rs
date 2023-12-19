e7053fe11814d84397e97c0b56afdf23c0a2f805
Thomas BESSOU <thomas.bessou@hotmail.fr>
2023-12-19 01:40:10 +0100

    move reader example to the Reader rustdoc

--- src/object_container_file_encoding/mod.rs
+++ src/object_container_file_encoding/mod.rs
@@ -3,43 +3,7 @@
 //! This is typically what you want when reading/writing avro files with
 //! multiple objects.
 //!
-//! # Reader example
-//! ```
-//! let avro_object_container_file_encoded: &[u8] = &[
-//! 	79, 98, 106, 1, 4, 22, 97, 118, 114, 111, 46, 115, 99, 104, 101, 109, 97, 222, 1, 123, 34,
-//! 	116, 121, 112, 101, 34, 58, 34, 114, 101, 99, 111, 114, 100, 34, 44, 34, 110, 97, 109, 101,
-//! 	34, 58, 34, 116, 101, 115, 116, 34, 44, 34, 102, 105, 101, 108, 100, 115, 34, 58, 91, 123,
-//! 	34, 110, 97, 109, 101, 34, 58, 34, 97, 34, 44, 34, 116, 121, 112, 101, 34, 58, 34, 108,
-//! 	111, 110, 103, 34, 44, 34, 100, 101, 102, 97, 117, 108, 116, 34, 58, 52, 50, 125, 44, 123,
-//! 	34, 110, 97, 109, 101, 34, 58, 34, 98, 34, 44, 34, 116, 121, 112, 101, 34, 58, 34, 115,
-//! 	116, 114, 105, 110, 103, 34, 125, 93, 125, 20, 97, 118, 114, 111, 46, 99, 111, 100, 101,
-//! 	99, 8, 110, 117, 108, 108, 0, 94, 61, 54, 221, 190, 207, 108, 180, 158, 57, 114, 40, 173,
-//! 	199, 228, 239, 4, 20, 54, 6, 102, 111, 111, 84, 6, 98, 97, 114, 94, 61, 54, 221, 190, 207,
-//! 	108, 180, 158, 57, 114, 40, 173, 199, 228, 239,
-//! ];
-//!
-//! #[derive(serde_derive::Deserialize, Debug, PartialEq, Eq)]
-//! struct SchemaRecord<'a> {
-//! 	a: i64,
-//! 	b: &'a str,
-//! }
-//!
-//! let mut reader = serde_avro_fast::object_container_file_encoding::Reader::from_slice(
-//! 	avro_object_container_file_encoded,
-//! )
-//! .expect("Failed to initialize reader");
-//!
-//! let expected = vec![
-//! 	SchemaRecord { a: 27, b: "foo" },
-//! 	SchemaRecord { a: 42, b: "bar" },
-//! ];
-//! let res: Vec<SchemaRecord> = reader
-//! 	.deserialize_borrowed::<SchemaRecord>()
-//! 	.collect::<Result<_, _>>()
-//! 	.expect("Failed to deserialize a record");
-//!
-//! assert_eq!(expected, res);
-//! ```
+//! See [`Reader`] and [`Writer`] documentations for their respective examples.
 
 mod reader;
 mod writer;
