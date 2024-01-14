29f67ddab38464e3aa319a17f203c0e4652a33a3
Thomas BESSOU <thomas.bessou@hotmail.fr>
2024-01-14 02:53:00 +0100

    wip

--- src/schema/safe/canonical_form.rs
+++ src/schema/safe/canonical_form.rs
@@ -18,12 +18,17 @@ impl EditableSchema {
 	/// reference Java implementation)
 	pub fn canonical_form_rabin_fingerprint(&self) -> Result<[u8; 8], SchemaError> {
 		let mut state = WriteCanonicalFormState {
-			w: UnwrappingWriter(Rabin::default()),
+			w: ErrorConversionWriter(Rabin::default()),
 			named_type_written: vec![false; self.nodes.len()],
 		};
 		state.write_canonical_form(self, SchemaKey::from_idx(0))?;
 		Ok(state.w.0.finish())
 	}
+}
+
+struct WriteCanonicalFormState<W> {
+	w: ErrorConversionWriter<W>,
+	named_type_written: Vec<bool>,
 }
 
 impl<W: Write> WriteCanonicalFormState<W> {
@@ -42,119 +47,139 @@ impl<W: Write> WriteCanonicalFormState<W> {
 			.ok_or_else(|| SchemaError::new("SchemaKey refers to non-existing node"))?;
 
 		let should_not_write_only_name =
-			|name: &s::Name, state: &mut WriteCanonicalFormState<W>| -> bool {
-				match &mut state.named_type_written[key.idx] {
+			|name: &s::Name, state: &mut WriteCanonicalFormState<W>| -> Result<bool, SchemaError> {
+				Ok(match &mut state.named_type_written[key.idx] {
 					b @ false => {
 						*b = true;
 						true
 					}
 					true => {
-						state.w.write_char('"');
-						state.w.write_str(name.fully_qualified_name());
-						state.w.write_char('"');
+						state.w.write_char('"')?;
+						state.w.write_str(name.fully_qualified_name())?;
+						state.w.write_char('"')?;
 						false
 					}
-				}
+				})
 			};
 
 		// In PCF, logical types are completely ignored
 		// https://issues.apache.org/jira/browse/AVRO-1721
 		match node.type_ {
 			SchemaType::Null => {
-				self.w.write_str("\"null\"");
+				self.w.write_str("\"null\"")?;
 			}
 			SchemaType::Boolean => {
-				self.w.write_str("\"boolean\"");
+				self.w.write_str("\"boolean\"")?;
 			}
 			SchemaType::Bytes => {
-				self.w.write_str("\"bytes\"");
+				self.w.write_str("\"bytes\"")?;
 			}
 			SchemaType::Double => {
-				self.w.write_str("\"double\"");
+				self.w.write_str("\"double\"")?;
 			}
 			SchemaType::Float => {
-				self.w.write_str("\"float\"");
+				self.w.write_str("\"float\"")?;
 			}
 			SchemaType::Int => {
-				self.w.write_str("\"int\"");
+				self.w.write_str("\"int\"")?;
 			}
 			SchemaType::Long => {
-				self.w.write_str("\"long\"");
+				self.w.write_str("\"long\"")?;
 			}
 			SchemaType::String => {
-				self.w.write_str("\"string\"");
+				self.w.write_str("\"string\"")?;
 			}
 			SchemaType::Union(ref union) => {
-				self.w.write_char('[');
+				self.w.write_char('[')?;
 				for &variant in &union.variants {
 					if !first_time {
-						self.w.write_char(',');
+						self.w.write_char(',')?;
 					} else {
 						first_time = false;
 					}
 					self.write_canonical_form(schema, variant)?;
 				}
-				self.w.write_char(']');
+				self.w.write_char(']')?;
 			}
 			SchemaType::Array(array_items) => {
-				self.w.write_str("{\"type\":\"array\",\"items\":");
+				self.w.write_str("{\"type\":\"array\",\"items\":")?;
 				self.write_canonical_form(schema, array_items)?;
-				self.w.write_char('}');
+				self.w.write_char('}')?;
 			}
 			SchemaType::Map(map_values) => {
-				self.w.write_str("{\"type\":\"map\",\"values\":");
+				self.w.write_str("{\"type\":\"map\",\"values\":")?;
 				self.write_canonical_form(schema, map_values)?;
-				self.w.write_char('}');
+				self.w.write_char('}')?;
 			}
 			SchemaType::Enum(ref enum_) => {
-				if !should_not_write_only_name(&enum_.name, self) {
-					self.w.write_str("{\"name\":\"");
-					self.w.write_str(enum_.name.fully_qualified_name());
-					self.w.write_str("\",\"type\":\"enum\",\"symbols\":[");
+				if !should_not_write_only_name(&enum_.name, self)? {
+					self.w.write_str("{\"name\":\"")?;
+					self.w.write_str(enum_.name.fully_qualified_name())?;
+					self.w.write_str("\",\"type\":\"enum\",\"symbols\":[")?;
 					for enum_symbol in enum_.symbols.iter() {
 						if !first_time {
-							self.w.write_char(',');
+							self.w.write_char(',')?;
 						} else {
 							first_time = false;
 						}
-						self.w.write_char('"');
-						self.w.write_str(enum_symbol);
-						self.w.write_char('"');
+						self.w.write_char('"')?;
+						self.w.write_str(enum_symbol)?;
+						self.w.write_char('"')?;
 					}
-					self.w.write_char(']');
-					self.w.write_char('}');
+					self.w.write_char(']')?;
+					self.w.write_char('}')?;
 				}
 			}
 			SchemaType::Fixed(ref fixed) => {
-				if !should_not_write_only_name(&fixed.name, self) {
-					self.w.write_str("{\"name\":\"");
-					self.w.write_str(fixed.name.fully_qualified_name());
-					self.w.write_str("\",\"type\":\"fixed\",\"size\":");
-					write!(self.w, "{}", fixed.size).unwrap();
-					self.w.write_char('}');
+				if !should_not_write_only_name(&fixed.name, self)? {
+					self.w.write_str("{\"name\":\"")?;
+					self.w.write_str(fixed.name.fully_qualified_name())?;
+					self.w.write_str("\",\"type\":\"fixed\",\"size\":")?;
+					write!(self.w.0, "{}", fixed.size).map_err(convert_error)?;
+					self.w.write_char('}')?;
 				}
 			}
 			SchemaType::Record(ref record) => {
-				if !should_not_write_only_name(&record.name, self) {
-					self.w.write_str("{\"name\":\"");
-					self.w.write_str(record.name.fully_qualified_name());
-					self.w.write_str("\",\"type\":\"record\",\"fields\":[");
+				if !should_not_write_only_name(&record.name, self)? {
+					self.w.write_str("{\"name\":\"")?;
+					self.w.write_str(record.name.fully_qualified_name())?;
+					self.w.write_str("\",\"type\":\"record\",\"fields\":[")?;
 					for field in record.fields.iter() {
 						if !first_time {
-							self.w.write_char(',');
+							self.w.write_char(',')?;
 						} else {
 							first_time = false;
 						}
-						self.w.write_str("{\"name\":\"");
-						self.w.write_str(&field.name);
-						self.w.write_str("\",\"type\":");
+						self.w.write_str("{\"name\":\"")?;
+						self.w.write_str(&field.name)?;
+						self.w.write_str("\",\"type\":")?;
 						self.write_canonical_form(schema, field.schema)?;
-						self.w.write_char('}');
+						self.w.write_char('}')?;
 					}
-					self.w.write_str("]}");
+					self.w.write_str("]}")?;
 				}
 			}
 		}
 		Ok(())
 	}
+}
+
+/// Convert errors from `std::fmt::Write` to `SchemaError`
+/// in order to be able to use `?` in `WriteCanonicalFormState`
+struct ErrorConversionWriter<W>(W);
+impl<W: Write> ErrorConversionWriter<W> {
+	#[inline]
+	fn write_char(&mut self, c: char) -> Result<(), SchemaError> {
+		self.0.write_char(c).map_err(convert_error)
+	}
+	#[inline]
+	fn write_str(&mut self, s: &str) -> Result<(), SchemaError> {
+		self.0.write_str(s).map_err(convert_error)
+	}
+}
+fn convert_error(e: std::fmt::Error) -> SchemaError {
+	SchemaError::msg(format_args!(
+		"Error writing schema parsing canonical form: {}",
+		e,
+	))
 }
